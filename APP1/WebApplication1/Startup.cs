6a381ffec1991ec4f84c5fb2e5dc81b5aaf6841b
Vinod Ramakrishnan <vinod.ramakrishnan@astadia.com>
2017-04-20 11:17:54 -0500

    test 1

--- APP1/WebApplication1/Startup.cs
+++ APP1/WebApplication1/Startup.cs
@@ -28,6 +28,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			//this is test by vinod
             // Add framework services.
             services.AddMvc();
         }
