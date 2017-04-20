98a7740ef6ef991dd1084f6c07c85c87864d760f
Vinod Ramakrishnan <Vinod.Ramakrishnan@astadia.com>
2017-04-20 10:55:26 -0500

    first change

--- APP1/WebApplication1/Startup.cs
+++ APP1/WebApplication1/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+//testc vinod
 namespace WebApplication1
 {
     public class Startup
